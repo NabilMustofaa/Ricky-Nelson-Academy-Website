22aed7aba8f77ca3b1cdcae476dc4ee248ed59cc
NabilMustofaa <nabilmustofa6@gmail.com>
2022-04-24 20:25:26 +0700

    new login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,38 +1,24 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
-    <title>{{ $title }}</title>
-    
-</head>
-<body>
-    <nav >
-        <div class="nav-container">
-            <a class="nav-text" href="/">Beranda</a>
-            <a class="nav-text" href="#">About Us</a>
-            <a class="nav-text" href="#">Jadwal</a>
-            <a class="nav-text" href="#">Artikel</a>
-            <a class="nav-text" href="/login">Login</a>
-        </div>
-    </nav>
-    <div class="content">
-        <h1>Halaman Login</h1>
-    </div>
+@extends('layouts.main')
+@include('partials.nav')
+@section('content')
 
     <form action="action_page.php" method="post">
-        <div class="login-container">
-          <label for="uname"><b>Username</b></label>
-          <input type="text" placeholder="Enter Username" name="uname" required>
-      
-          <label for="psw"><b>Password</b></label>
-          <input type="password" placeholder="Enter Password" name="psw" required>
-      
-          {{-- <button type="submit">Login</button> --}}
-          <a class="button" href="/dashboard">Login</a>
+    <div class="container d-flex shadow-lg w-50 h-75 align-items-center p-0 mt-5">
+        <div class="d-flex w-50 h-100 m-0 p-5 align-items-center" style="background-color: #04AA6D;">
+            <H1 style="color: white">Ricky Nelson Academy login</H1>
         </div>
+        <div class="d-flex flex-column login-container m-5 w-50">
+
+            <label for="uname"><b class="h5 mb-3">Username</b></label>
+            <input class="form-control mb-3" type="text" placeholder="Enter Username" name="uname" required>
+        
+            <label for="psw"><b class="h5  mb-3">Password</b></label>
+            <input class="form-control  mb-3" type="password" placeholder="Enter Password" name="psw" required>
+        
+            {{-- <button type="submit">Login</button> --}}
+            <a class="btn mb-3 w-100" style="background-color: #04AA6D; color:white" href="/dashboard">Login</a>
+          </div>
+    </div>    
+        
       </form>
-</body>
-</html>
+@endsection
